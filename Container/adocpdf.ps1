ed8635fff16a941c2ef91981fea6fa1a6c84d894
Travis Michette <tmichett@redhat.com>
2024-05-04 15:16:38 -0400

    Initial checkin and configuration of REPO

--- Container/adocpdf.ps1
+++
@@ -1,7 +0,0 @@
-$book_arg=$args[0]
-Write-Output $book_arg
-$container = "quay.io/tmichett/asciidocpdf:latest"
-$current_directory = (get-location).path
-$pdf_theme="-a pdf-theme=/opt/asciidoc/themes/tm-gls-redhat-theme.yml"
-Write-Output $current_directory
-Invoke-Expression "docker run -it --name adocpdf --rm -e 'pdf_theme=$pdf_theme' -e 'book_name=$book_arg'-v $current_directory`:/tmp/coursebook $container"
